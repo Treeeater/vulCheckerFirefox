f10bde328926843fbec6985aead8fbbda4fdb92f
Yuchen Zhou <yz8ra@virginia.edu>
2013-05-28 12:12:00 -0400

    fixed a bug that checkToken is not exported.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A vul checker application.
+
+Note:
+Firefox needs to have popup blocking turned off (go to options to diable that)
+and also caching turned off (got to about:config and type network.http.use-cache to disable it).
--- data/element-getter.js
+++
@@ -1,7 +0,0 @@
-self.port.on("getElements", function(tag) {
-  var elements = document.getElementsByTagName(tag);
-  for (var i = 0; i < elements.length; i++) {
-    self.port.emit("gotElement", elements[i].innerHTML);
-  }
-});
-console.log('element-getter.js loaded');
