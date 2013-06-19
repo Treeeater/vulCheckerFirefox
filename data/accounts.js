7705b07b99a0eef3509016a5f85a9fc467574b82
Yuchen Zhou <yz8ra@virginia.edu>
2013-06-19 18:01:43 -0400

    1.0a update.

--- data/accounts.js
+++ data/accounts.js
@@ -12,7 +12,8 @@ var accounts = [];
 
 
 //Use all lower case please:
+//no http protocol in picsrc please, because sites might use https.
 
-//accounts.push(new Account("yuchentest","zhou","t-yuzhou@hotmail.com","372678_100003929906137_108293698_q.jpg","10468_169575083183487_845810054_n.jpg","http://graph.facebook.com/yuchentest.zhou/picture","http://graph.facebook.com/100003929906137/picture"));
-accounts.push(new Account("syxvq","ldswpk","syxvq_ldswpk@yahoo.com","211884_100006061110883_1974931350_q.jpg","971984_1374544449424246_1607036321_n.jpg","http://graph.facebook.com/syxvq.ldswpk/picture","http://graph.facebook.com/100006061110883/picture"));
-accounts.push(new Account("chishi","wochishi","chishiwo@yahoo.com","372654_100006188405019_233762707_q.jpg","969187_1376813269201607_843083776_n.jpg","http://graph.facebook.com/chishi.wo/picture","http://graph.facebook.com/100006188405019/picture"));
+//accounts.push(new Account("yuchentest","zhou","t-yuzhou@hotmail.com","372678_100003929906137_108293698_q.jpg","10468_169575083183487_845810054_n.jpg","graph.facebook.com/yuchentest.zhou/picture","graph.facebook.com/100003929906137/picture"));
+accounts.push(new Account("syxvq","ldswpk","syxvq_ldswpk@yahoo.com","211884_100006061110883_1974931350_q.jpg","971984_1374544449424246_1607036321_n.jpg","graph.facebook.com/syxvq.ldswpk/picture","graph.facebook.com/100006061110883/picture"));
+accounts.push(new Account("chishi","wochishi","chishiwo@yahoo.com","372654_100006188405019_233762707_q.jpg","969187_1376813269201607_843083776_n.jpg","graph.facebook.com/chishi.wo/picture","graph.facebook.com/100006188405019/picture"));
