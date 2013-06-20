92f8c030037ba9e6b0c7569c2f6af68765ab4f4c
Yuchen Zhou <yz8ra@virginia.edu>
2013-06-20 12:44:34 -0400

    account info updated, more debug info, TODO: fix CheckToken:489 to capturingURLs, 1.0.1a

--- data/accounts.js
+++ data/accounts.js
@@ -16,4 +16,5 @@ var accounts = [];
 
 //accounts.push(new Account("yuchentest","zhou","t-yuzhou@hotmail.com","372678_100003929906137_108293698_q.jpg","10468_169575083183487_845810054_n.jpg","graph.facebook.com/yuchentest.zhou/picture","graph.facebook.com/100003929906137/picture"));
 accounts.push(new Account("syxvq","ldswpk","syxvq_ldswpk@yahoo.com","211884_100006061110883_1974931350_q.jpg","971984_1374544449424246_1607036321_n.jpg","graph.facebook.com/syxvq.ldswpk/picture","graph.facebook.com/100006061110883/picture"));
-accounts.push(new Account("chishi","wochishi","chishiwo@yahoo.com","372654_100006188405019_233762707_q.jpg","969187_1376813269201607_843083776_n.jpg","graph.facebook.com/chishi.wo/picture","graph.facebook.com/100006188405019/picture"));
+//accounts.push(new Account("chishi","wochishi","chishiwo@yahoo.com","372654_100006188405019_233762707_q.jpg","969187_1376813269201607_843083776_n.jpg","graph.facebook.com/chishi.wo/picture","graph.facebook.com/100006188405019/picture"));
+accounts.push(new Account("oiaeai","cktstltauoai","oiaeaicktstltauoai@yahoo.com","211864_100006154000685_1779045138_q.jpg","1000284_1381272638754523_1690689043_n.jpg","graph.facebook.com/oiaeai.cktstltauoai/picture","graph.facebook.com/100006154000685/picture"));
