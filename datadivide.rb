73746f3d2ddb90228d2845c8da7bd43a3764ab90
Yuchen Zhou <yz8ra@virginia.edu>
2013-07-27 21:13:45 -0400

    typo fix

--- datadivide.rb
+++ datadivide.rb
@@ -13,7 +13,7 @@ end
 for i in 0..totalFileNumber-2
 	File.open("dataset\\testSet"+(i+1).to_s+".js","w"){|fh|
 		fh.write("//This test set includes site No. #{i*500} to #{i*500+499}.\n")
-		fh.write("exports.testLists = [")
+		fh.write("exports.testList = [")
 		for j in 0..498
 			fh.write("'"+sitesToTest[i*500+j]+"',")
 		end
@@ -22,7 +22,7 @@ for i in 0..totalFileNumber-2
 end
 File.open("dataset\\testSet"+(i+2).to_s+".js","w"){|fh|
 	fh.write("//This test set includes site No. #{(i+1)*500} to #{(i+1)*500+sitesToTest.length%500}.\n")
-	fh.write("exports.testLists = [")
+	fh.write("exports.testList = [")
 	for j in 0..sitesToTest.length%500-2
 		fh.write("'"+sitesToTest[(i+1)*500+j]+"',")
 	end
