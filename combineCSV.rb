60d5d26d093b95be35d63e6f96fcad5141b04841
Yuchen Zhou <yz8ra@virginia.edu>
2013-09-08 00:52:51 -0400

    added error state combine.

--- combineCSV.rb
+++ combineCSV.rb
@@ -1,3 +1,11 @@
+def setSiteToError(hash,siteURL)
+	hash[siteURL][0] = 2
+	hash[siteURL][1] = 2
+	hash[siteURL][2] = 2
+	hash[siteURL][3] = 2
+	hash[siteURL][4] = 2
+end
+
 if (ARGV.length != 2)
 	p "wrong number of arguments. needs 1"
 	exit 
@@ -29,25 +37,45 @@ text2.each_line do |line|
 	if (hash[siteURL]==nil) then hash[siteURL] = Array.new end
 	
 	if ((hash[siteURL])[0] != "" && (hash[siteURL])[0] != nil && tempArray[1] != "" && tempArray[1] != nil && (hash[siteURL])[0] != tempArray[1])
+		if (tempArray[1] == "2")
+			setSiteToError(hash,siteURL)
+			next
+		end
 		if (errorHash[siteURL] == nil) then errorHash[siteURL] = 0 end
 		errorHash[siteURL] += 1
 	end
 	if ((hash[siteURL])[1] != "" && (hash[siteURL])[1] != nil && tempArray[2] != "" && tempArray[2] != nil && (hash[siteURL])[1] != tempArray[2]) 
+		if (tempArray[2] == "2")
+			setSiteToError(hash,siteURL)
+			next
+		end
 		if (errorHash[siteURL] == nil) then errorHash[siteURL] = 0 end
 		errorHash[siteURL] += 2
 	end
 	if ((hash[siteURL])[2] != "" && (hash[siteURL])[2] != nil && tempArray[3] != "" && tempArray[3] != nil && (hash[siteURL])[2] != tempArray[3]) 
+		if (tempArray[3] == "2")
+			setSiteToError(hash,siteURL)
+			next
+		end
 		if (errorHash[siteURL] == nil) then errorHash[siteURL] = 0 end
 		errorHash[siteURL] += 4
 	end
 	if ((hash[siteURL])[3] != "" && (hash[siteURL])[3] != nil && tempArray[4] != "" && tempArray[4] != nil && (hash[siteURL])[3] != tempArray[4]) 
 		#Disagreement here is fine, we probably didn't see that in one try. Mark it vulnerable
 		#p siteURL + " changed to vulnerable on disagreement for 4"
+		if (tempArray[4] == "2")
+			setSiteToError(hash,siteURL)
+			next
+		end
 		hash[siteURL][3] = -1
 	end
 	if ((hash[siteURL])[4] != "" && (hash[siteURL])[4] != nil && tempArray[5].chomp != "" && tempArray[5].chomp != nil && (hash[siteURL])[4] != tempArray[5].chomp) 
 		#Disagreement here is fine, we probably didn't see that in one try. Mark it vulnerable
 		#p siteURL + " changed to vulnerable on disagreement for 5"
+		if (tempArray[5] == "2")
+			setSiteToError(hash,siteURL)
+			next
+		end
 		hash[siteURL][4] = -1
 	end
 	
@@ -70,15 +98,19 @@ errorHash.each_key{|k|
 outputText = "Site URL,token vul,secret vul,signed_request vul,referrer vul,DOM vul\n"
 completedCases = 0
 failedCasesToTestNext = Array.new
+allSitesCount = 0
 hash.each_key{|k|
 	if (hash[k][0].to_s!="" && hash[k][1].to_s!="" && hash[k][2].to_s!="" && hash[k][3].to_s!="" && hash[k][4].to_s!="") 
 		completedCases+=1 
 	else
 		failedCasesToTestNext.push(k)
 	end
+	allSitesCount+=1
 	outputText = outputText + k + ',' + hash[k][0].to_s + ',' + hash[k][1].to_s + ',' + hash[k][2].to_s + ',' + hash[k][3].to_s + ',' + hash[k][4].to_s + "\n"
 }
+p "total tests: #{allSitesCount}"
 p "total completed tests: #{completedCases}"
+p "total failed tests: #{allSitesCount - completedCases}"
 File.open("Results_new.csv","w+"){|f|
 	f.write(outputText)
 }
