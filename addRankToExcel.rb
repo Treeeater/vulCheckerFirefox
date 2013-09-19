d648693945bbfa5943090094714efd81648882b0
Yuchen Zhou <yz8ra@virginia.edu>
2013-09-19 10:55:01 -0400

    updates to addRankToExcel.rb

--- addRankToExcel.rb
+++ addRankToExcel.rb
@@ -1,5 +1,5 @@
 if (ARGV.length != 2)
-	p "wrong number of arguments. needs 2"
+	p "wrong number of arguments. needs 2, first: quantcast list. second: result.csv"
 	exit 
 end
 
@@ -14,22 +14,61 @@ rankFile.each_line{|l|
 }
 rankedArrayToWrite = Hash.new
 percentileArray = Array.new
+vul13PercentileArray = Array.new
+vul45PercentileArray = Array.new
 resultFile.each_line{|l|
-	site = l.split(',')[0]
+	temp = l.chomp.split(',')
+	site = temp[0]
+	vul13 = (temp[1] == '-1' || temp[3] == '-1')
+	vul45 = (temp[4] == '-1' || temp[5] == '-1')
 	index = rankArray.index(site[11..-1])
 	if (index == nil) 
 		p l
 	else
 		rankedArrayToWrite[index] = l
 		if (percentileArray[index*100/totalSites] == nil) then percentileArray[index*100/totalSites] = 1 else percentileArray[index*100/totalSites] += 1 end
+		if vul13
+			if (vul13PercentileArray[index*100/totalSites] == nil) then vul13PercentileArray[index*100/totalSites] = 1 else vul13PercentileArray[index*100/totalSites] += 1 end
+		end
+		if vul45
+			if (vul45PercentileArray[index*100/totalSites] == nil) then vul45PercentileArray[index*100/totalSites] = 1 else vul45PercentileArray[index*100/totalSites] += 1 end
+		end
 	end
 }
 
 percentileFileContent = ""
 rankedFileContent = ""
 
-percentileArray.each{|p|
-	percentileFileContent += (p.to_s + ",")
+percentileArray.each_index{|i|
+	if !percentileArray[i] then percentileArray[i] = 0 end
+	percentileFileContent += (percentileArray[i].to_s + ",")
+}
+percentileFileContent += "\n"
+
+vul13PercentileArray.each_index{|i|
+	if !vul13PercentileArray[i] then vul13PercentileArray[i] = 0 end
+	percentileFileContent += (vul13PercentileArray[i].to_s + ",")
+}
+
+percentileFileContent += "\n"
+
+vul45PercentileArray.each_index{|i|
+	if !vul45PercentileArray[i] then vul45PercentileArray[i] = 0 end
+	percentileFileContent += (vul45PercentileArray[i].to_s + ",")
+}
+
+percentileFileContent += "\n"
+
+vul13PercentileArray.each_index{|i|
+	if (percentileArray[i] == 0) then percentileFileContent += "0," end
+	percentileFileContent += ((vul13PercentileArray[i]/percentileArray[i].to_f).to_s + ",")
+}
+
+percentileFileContent += "\n"
+
+vul45PercentileArray.each_index{|i|
+	if (percentileArray[i] == 0) then percentileFileContent += "0," end
+	percentileFileContent += ((vul45PercentileArray[i]/percentileArray[i].to_f).to_s + ",")
 }
 
 rankedArrayToWrite.sort.map do |key,value|
