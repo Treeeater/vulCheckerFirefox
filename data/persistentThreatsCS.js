5e81d62d59639b6f0b4801152bcd505a2b6a2b89
Yuchen Zhou <yz8ra@virginia.edu>
2014-05-20 17:20:00 -0400

    bug fix for misclassifying sites with access_token in fragments as vulnerable.

--- data/persistentThreatsCS.js
+++ data/persistentThreatsCS.js
@@ -61,7 +61,7 @@ function initDetection(credentials){
 	findThirdPartyScript(document.documentElement);
 	findThirdPartyContent(document.documentElement);
 
-	var url = document.URL;
+	var url = document.URL.substr(0,document.URL.indexOf('#'));			//fragments are not visible in referer header! Thanks Eugene!
 	var html = document.documentElement.innerHTML;
 	
 	if (hasThirdPartyContent){
